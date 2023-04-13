7f9b74d22b5e97c84eba08562d47a9cc83fd9b29
pratikg-tagline <pratikg.tagline@gmail.com>
2023-04-13 16:47:03 +0530

    implement first vvvv

---
+++ src/app/auth/firstc/firstc.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-firstc',
+  templateUrl: './firstc.component.html',
+  styleUrls: ['./firstc.component.scss']
+})
+export class FirstcComponent {
+
+}
